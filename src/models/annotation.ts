c34a2a2b59bcdfea6728c13ea09a77234fde2b09
Patrick Tasse <patrick.tasse@gmail.com>
2020-11-20 16:03:35 -0500

    Add annotation model and tsp-client API for fetching annotations
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>

---
+++ src/models/annotation.ts
@@ -0,0 +1,50 @@
+import { OutputElementStyle } from './styles';
+
+export enum Type {
+    TREE = 'TREE',
+    CHART = 'CHART'
+}
+
+export interface AnnotationCategoriesModel {
+    annotationCategories: string[];
+}
+
+export interface AnnotationModel {
+    annotations: { [category: string]: Annotation[] };
+}
+
+/**
+ * Model for annotation
+ */
+export interface Annotation {
+
+    /**
+     * Label of the annotation
+     */
+    label: string;
+
+    /**
+     * Time of the annotation
+     */
+    time: number;
+
+    /**
+     * Duration of the annotation
+     */
+    duration: number;
+
+    /**
+     * Entry Id of the annotation
+     */
+    entryId: number;
+
+    /**
+     * Type of the annotation
+     */
+    type: string;
+
+    /**
+     * Style of the annotation
+     */
+    style?: OutputElementStyle;
+}
